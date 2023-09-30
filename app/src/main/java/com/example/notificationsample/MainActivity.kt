5435b07eaa3a419417f89d4901f1d1775bde192f
ijaewon <ljw14@naonsoft.com>
2023-10-01 01:09:20 +0900

    - Upgrade for targetSDK 33, - Add summary notification, - Add notification with action buttons.

--- app/src/main/java/com/example/notificationsample/MainActivity.kt
+++ app/src/main/java/com/example/notificationsample/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.example.notificationsample
 
+import android.Manifest
 import android.app.NotificationManager
 import android.content.ContextWrapper
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.example.notificationsample.databinding.ActivityMainBinding
 
@@ -19,16 +25,63 @@ class MainActivity : AppCompatActivity() {
         //ViewBinding 연결
         setContentView(binding.root)
 
+        // Notification action 통해 들어온 intent 처리
+        intent?.getBooleanExtra("action", false)?.let {
+            if(it)
+                Toast.makeText(this, "Action from notification", Toast.LENGTH_SHORT).show()
+        }
+
+        checkPermission()
+
         var count : Int = 0
-        binding.notyButton.run {
-            setOnClickListener {
-                NotifyHelper.notify(this@MainActivity, getString(R.string.app_name), "TEST", NotifyHelper.NOTIFY_TYPE.BASIC)
+        binding.apply {
+            notyButton.run {
+                setOnClickListener {
+                    NotifyHelper.notify(this@MainActivity, getString(R.string.app_name), "TEST", NotifyHelper.NOTIFY_TYPE.BASIC)
+                }
+
+                setOnLongClickListener {
+                    NotifyHelper.notify(this@MainActivity, getString(R.string.app_name), "Big Picture Test", NotifyHelper.NOTIFY_TYPE.PICTURE)
+                    false
+                }
             }
 
-            setOnLongClickListener {
-                NotifyHelper.notify(this@MainActivity, getString(R.string.app_name), "Big Picture Test", NotifyHelper.NOTIFY_TYPE.PICTURE)
-                false
+            btnActionNotify.setOnClickListener {
+                NotifyHelper.notify(this@MainActivity, getString(R.string.app_name), "ACTION BUTTON TEST", NotifyHelper.NOTIFY_TYPE.ACTION)
             }
         }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        intent?.getBooleanExtra("action", false)?.let {
+            if(it)
+                Toast.makeText(this, "Action from notification", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+//    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun checkPermission() {
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) return
+        if(checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(Manifest.permission.POST_NOTIFICATIONS), 9999)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        permissions.forEachIndexed { index, permission ->
+            if(grantResults[index] != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this@MainActivity, "권한이 없으면 앱을 실행할 수 없어요.", Toast.LENGTH_SHORT).show()
+                finishAffinity()
+            }
+        }
+
     }
 }
--- app/src/main/java/com/example/notificationsample/NotifyHelper.kt
+++ app/src/main/java/com/example/notificationsample/NotifyHelper.kt
@@ -12,27 +12,39 @@ import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.getSystemService
+import kotlin.random.Random
 
 object NotifyHelper {
-    private const val NOTIFICATION_ID = 0
     private const val CHANNEL_ID = "NOTIFICATION TEMPLATE"
     enum class NOTIFY_TYPE(num: Int) {
-        BASIC(0), PICTURE(1)
+        BASIC(0), PICTURE(1), ACTION(2)
     }
+
+    private const val GROUP = "NOTIFICATION TEST GROUP"
+    private const val SUMMARY_ID = 10001
+    private var NOTIFICATION_ID = 0
 
     fun notify(context: Context, title: String, message: String, type: NOTIFY_TYPE) {
         val notificationManager = context.getSystemService(NotificationManager::class.java)
         createNotificationChannel(notificationManager)
+        NOTIFICATION_ID = makeNotificationID()
         val builder = when(type) {
             NOTIFY_TYPE.BASIC -> getNotificationBuilder(context, title, message)
 //            NOTIFY_TYPE.PICTURE -> getPictureNotificationBuilder(context, title, message, Bitmap())
+            NOTIFY_TYPE.ACTION -> getActionNotificationBuilder(context, title, message)
             else -> NotificationCompat.Builder(context, CHANNEL_ID)
         }
 
-        notificationManager.notify(
-            NOTIFICATION_ID,
-            builder.build()
-        )
+        notificationManager.run {
+            notify(
+                NOTIFICATION_ID,
+                builder.build()
+            )
+            notify(
+                SUMMARY_ID,
+                getSummaryNotificationBuilder(context, title, message).build()
+            )
+        }
     }
 
     private fun createNotificationChannel(notificationManager: NotificationManager) {
@@ -63,6 +75,7 @@ object NotifyHelper {
             setSmallIcon(R.drawable.ic_launcher_foreground)
             setContentIntent(notificationPendingIntent)
             setAutoCancel(true)
+            setGroup(GROUP)
         }
     }
 
@@ -83,4 +96,27 @@ object NotifyHelper {
             setAutoCancel(true)
         }
     }
+
+    private fun getActionNotificationBuilder(context: Context, title: String, text: String) : NotificationCompat.Builder {
+        val mIntent = Intent(context, MainActivity::class.java).apply { putExtra("action", true) }
+        val pendingIntent = PendingIntent.getActivity(
+            context,
+            NOTIFICATION_ID,
+            mIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+        return getNotificationBuilder(context, title, text)
+                .addAction(R.drawable.ic_launcher_foreground, "ACTION!", pendingIntent)
+    }
+
+    private fun getSummaryNotificationBuilder(context: Context, title: String, text: String) : NotificationCompat.Builder {
+        return NotificationCompat.Builder(context, CHANNEL_ID)
+            .setContentTitle(title)
+            .setContentText(text)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setGroup(GROUP)
+            .setGroupSummary(true)
+    }
+
+    private fun makeNotificationID() = Random.nextInt(9999)
 }
